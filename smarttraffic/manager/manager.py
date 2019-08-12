2b348fcb271230c0cc3a98fa63a8d3c1d9903148
Victor Vuelma <victorvuelma@outlook.com>
2019-08-11 23:33:14 -0300

    manager refactor

--- smarttraffic/manager/manager.py
+++ smarttraffic/manager/manager.py
@@ -1,18 +1,32 @@
-from abc import ABC, abstractmethod
 from enum import Enum
+class Manager(object):
 
-class Manager(ABC):
+    def __construct__(self):
+        self.state = State.WAITING
 
-    @abstractmethod
     def init(self):
+        if(self.state is State.WAITING):
+            self.init_manager()
+
+        if(self.state is State.WAITING or self.state is State.STOPPED ):
+            self.end_manager()
+            self.state = State.RUNNING
+
+    def end(self):
+        if(self.state is State.RUNNING or self.state is State.STOPPED):
+            self.end_manager()
+
+    def init_manager(self):
         pass
 
-    @abstractmethod
-    def end(self):
+    def start_manager(self):
+        pass
+
+    def end_manager(self):
         pass
 
 class State(Enum):
-    NONE = 0
+    WAITING = 0
     RUNNING = 1
     STOPPED = 2
     ENDED = 3
