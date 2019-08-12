64d2d6f64143a07c67bb8ede11b34c3b0670593c
Victor Vuelma <victorvuelma@outlook.com>
2019-08-12 00:47:04 -0300

    refactor system manager to use tasks

--- smarttraffic/manager/system.py
+++ smarttraffic/manager/system.py
@@ -1,39 +1,45 @@
+from time import sleep
 import multitasking
 from gpiozero import CPUTemperature, LoadAverage
-from time import sleep
+from termcolor import cprint
 
 from smarttraffic.manager import manager
+from smarttraffic.manager import task
 
+class SystemMonitorTask(task.Task):
+
+    def __init__(self, id):
+        super().__init__(id, 15, True)
+
+    def execute(self):
+        system_manager.send_monitor()
 
 class SystemManager(manager.Manager):
 
     def __init__(self):
+        super().__init__()
+
+    def init_manager(self):
         pass
+    
+    def start_manager(self):
+        self.start_monitor()
 
-    def init(self):
-        self.startMonitor()
+    def end_manager(self):
+        self.end_monitor()
 
-    def end(self):
-        pass
-
-    @multitasking.task
-    def startMonitor(self):
-        self._runMonitor = True
-
-        while(self._runMonitor):
-            self.sendMonitor()
+    def start_monitor(self):
+        task.task_manager.create_task(SystemMonitorTask('system_monitor'))
             
-            if(self._runMonitor): sleep(15)
-            
-    def endMonitor(self):
-        self._runMonitor = False
+    def end_monitor(self):
+        task.task_manager.end_task('system_monitor')
 
-    def sendMonitor(self):
+    def send_monitor(self):
         _cpu = CPUTemperature()
         _load = LoadAverage()
 
-        print('[SYSTEM MGR] Current system status:')
-        print('[CPU Temperature] %0.2f ºC' % (_cpu.temperature))
-        print(f'[LOAD] {_load.load_average}')
+        cprint('[SYSTEM MGR] Current system status:', 'yellow')
+        cprint('[CPU Temperature] %0.2f ºC' % (_cpu.temperature), 'red')
+        cprint(f'[LOAD] {_load.load_average}', 'green')
 
-manager = SystemManager()        
+system_manager = SystemManager()        
