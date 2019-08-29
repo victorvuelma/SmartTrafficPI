e01ea2fae8ed1a76ca3d8bf20fd413c72b961ac8
Victor Vuelma <victorvuelma@outlook.com>
2019-08-29 14:28:34 -0300

    use dotenv to check and run out of raspberry

--- smarttraffic/manager/system.py
+++ smarttraffic/manager/system.py
@@ -1,10 +1,12 @@
 from time import sleep
-import multitasking
+from os import getenv
+
 from gpiozero import CPUTemperature, LoadAverage
 from termcolor import cprint
 
 from smarttraffic.manager import manager
 from smarttraffic.manager import task
+
 
 class SystemMonitorTask(task.Task):
 
@@ -14,6 +16,7 @@ class SystemMonitorTask(task.Task):
     def execute(self):
         system_manager.send_monitor()
 
+
 class SystemManager(manager.Manager):
 
     def __init__(self):
@@ -21,7 +24,7 @@ class SystemManager(manager.Manager):
 
     def init_manager(self):
         pass
-    
+
     def start_manager(self):
         self.start_monitor()
 
@@ -30,16 +33,18 @@ class SystemManager(manager.Manager):
 
     def start_monitor(self):
         task.task_manager.create_task(SystemMonitorTask('system_monitor'))
-            
+
     def end_monitor(self):
         task.task_manager.end_task('system_monitor')
 
     def send_monitor(self):
-        _cpu = CPUTemperature()
-        _load = LoadAverage()
-
         cprint('[SYSTEM MGR] Current system status:', 'yellow')
-        cprint('[CPU Temperature] %0.2f ºC' % (_cpu.temperature), 'red')
-        cprint(f'[LOAD] {_load.load_average}', 'green')
 
-system_manager = SystemManager()        
+        if(getenv('raspberry') == True):
+            _cpu = CPUTemperature()
+            _load = LoadAverage()
+            cprint('[CPU Temperature] %0.2f ºC' % (_cpu.temperature), 'red')
+            cprint(f'[LOAD] {_load.load_average}', 'green')
+
+
+system_manager = SystemManager()
