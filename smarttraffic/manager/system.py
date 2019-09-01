7cdc291b2e0bc40d7c955741f6598ef5069538d0
Victor Vuelma <victorvuelma@outlook.com>
2019-09-01 01:08:15 -0300

    add time running to system monitor

--- smarttraffic/manager/system.py
+++ smarttraffic/manager/system.py
@@ -1,5 +1,6 @@
 from time import sleep
 from os import getenv
+from datetime import datetime, timedelta
 
 from gpiozero import CPUTemperature, LoadAverage
 from termcolor import cprint
@@ -26,6 +27,10 @@ class SystemManager(manager.Manager):
         self.init_monitor()
 
     def start_manager(self):
+        self.started = datetime.now()
+
+        print('started ok')
+
         self.start_monitor()
 
     def end_manager(self):
@@ -40,14 +45,41 @@ class SystemManager(manager.Manager):
     def end_monitor(self):
         task.task_manager.end_task('system_monitor')
 
+    def td_format(self, td_object):
+        seconds = int(td_object.total_seconds())
+        periods = [
+            ('y',        60*60*24*365),
+            ('M',       60*60*24*30),
+            ('d',         60*60*24),
+            ('h',        60*60),
+            ('m',      60),
+            ('s',      1)
+        ]
+
+        strings = []
+        for period_name, period_seconds in periods:
+            if seconds > period_seconds:
+                period_value, seconds = divmod(seconds, period_seconds)
+                strings.append("%s%s" % (period_value, period_name))
+        if strings.__len__ == 0:
+            return '1s'
+
+        return "".join(strings)
+
     def send_monitor(self):
         cprint('[SYSTEM MGR] Current system status:', 'yellow')
+
+        now = datetime.now()
+        running = now - self.started
+        running = self.td_format(running)
 
         if(getenv('raspberry') == True):
             _cpu = CPUTemperature()
             _load = LoadAverage()
             cprint('[CPU Temperature] %0.2f ºC' % (_cpu.temperature), 'red')
             cprint(f'[LOAD] {_load.load_average}', 'green')
+
+        cprint(f'[TIME] Running for {running}', 'yellow')
 
 
 system_manager = SystemManager()
