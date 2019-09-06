a318e4f4488a8e6bde5a4dabac91536c95e9dbaa
victorvuelma <victorvuelma@outlook.com>
2019-09-06 00:00:21 -0300

    prepare for raspberry

--- smarttraffic/smarttraffic.py
+++ smarttraffic/smarttraffic.py
@@ -24,6 +24,7 @@ def init():
     cprint('ST => Initializing...', 'yellow')
     task_manager.init()
     system_manager.init()
+    device_manager.init()
     network_manager.init()
     traffic_manager.init()
     cprint('ST => Init complete.', 'green')
@@ -33,6 +34,7 @@ def init():
     system_manager.start()
     network_manager.start()
     traffic_manager.start()
+    device_manager.start()
     cprint('ST => Start complete.', 'green')
 
     light_paulista_a = trafficlight.TrafficLight(
@@ -109,59 +111,39 @@ def init():
 
             cross_road_av_paulista_b.request_open()
 
-    if(getenv('RASPBERRY') == True):
-        loadSys()
+    if getenv('RASPBERRY') is True:
+        sensor_bela_cintra = trafficsensor_device.TrafficSensorDevice(
+            'bela_cintra', 4)
+        sensor_paulista_a = trafficsensor_device.TrafficSensorDevice(
+            'paulista_a', 3)
+        sensor_paulista_b = trafficsensor_device.TrafficSensorDevice(
+            'paulista_b', 2)
 
+        light_bela_cintra = trafficlight_device.TrafficLightDevice(
+            'bela_cintra', 16, 20, 21)
+        light_paulista_a = trafficlight_device.TrafficLightDevice(
+            'paulista_a', 14, 15, 18)
+        light_paulista_b = trafficlight_device.TrafficLightDevice(
+            'paulista_b', 25, 8, 7)
 
-def loadSys():
-    sensor_bela_cintra = trafficsensor_device.TrafficSensorDevice(
-        'bela_cintra', 4)
-    sensor_paulista_a = trafficsensor_device.TrafficSensorDevice(
-        'paulista_a', 3)
-    sensor_paulista_b = trafficsensor_device.TrafficSensorDevice(
-        'paulista_b', 2)
+        devices = [sensor_bela_cintra, sensor_paulista_a, sensor_paulista_b,
+            light_bela_cintra, light_paulista_a, light_paulista_b]
+        for device in devices:
+            device_manager.link_device(device)
 
-    light_bela_cintra = trafficlight_device.TrafficLightDevice(
-        'bela_cintra', 16, 20, 21)
-    light_paulista_a = trafficlight_device.TrafficLightDevice(
-        'paulista_a', 14, 15, 18)
-    light_paulista_b = trafficlight_device.TrafficLightDevice(
-        'paulista_b', 25, 8, 7)
+        cross_road_bela_cintra.link_traffic_light(light_bela_cintra)
 
-    devices = [sensor_bela_cintra, sensor_paulista_a, sensor_paulista_b,
-               light_bela_cintra, light_paulista_a, light_paulista_b]
+        cross_road_av_paulista_a.link_traffic_light(light_paulista_a)
 
-    device_manager.init()
+        cross_road_av_paulista_b.link_traffic_light(light_paulista_b)
 
-    cross_paulista_bela_cintra = crossing.Crossing()
-
-    road_av_paulista = road.TwoWaysRoad('av_paulista', 'Av. Paulista')
-    road_bela_cintra = road.Road('rua_bela_cintra', 'Rua Bela Cintra')
-
-    cross_road_bela_cintra = crossing.CrossingRoad(road_bela_cintra)
-    cross_road_bela_cintra.link_traffic_light(light_bela_cintra)
-
-    cross_road_av_paulista_a = crossing.CrossingRoad(road_av_paulista.way_a)
-    cross_road_av_paulista_a.link_traffic_light(light_paulista_a)
-
-    cross_road_av_paulista_b = crossing.CrossingRoad(road_av_paulista.way_b)
-    cross_road_av_paulista_b.link_traffic_light(light_paulista_b)
-
-    cross_paulista_bela_cintra.add_road(cross_road_bela_cintra)
-    cross_paulista_bela_cintra.add_road(cross_road_av_paulista_a)
-
-    cross_paulista_bela_cintra.add_cross(
-        cross_road_av_paulista_a, cross_road_bela_cintra)
-    cross_paulista_bela_cintra.add_cross(
-        cross_road_av_paulista_b, cross_road_bela_cintra)
-
-    while(True):
-        if sensor_bela_cintra.find():
-            print('open bela cintra')
-            cross_road_bela_cintra.request_open()
-        elif sensor_paulista_a.find():
-            print('open paulista a')
-            cross_road_av_paulista_a.request_open()
-        elif sensor_paulista_b.find():
-            print('open paulista b')
-            cross_road_av_paulista_b.request_open()
+        while(True):
+            if sensor_bela_cintra.find():
+                print('open bela cintra')
+                cross_road_bela_cintra.request_open()
+            elif sensor_paulista_a.find():
+                print('open paulista a')
+                cross_road_av_paulista_a.request_open()
+            elif sensor_paulista_b.find():
+                print('open paulista b')
+                cross_road_av_paulista_b.request_open()
